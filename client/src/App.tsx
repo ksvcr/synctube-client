bc07056af185391f3d5f67c8202640ab1796f504
kuklinsv <kuklinsv@yandex-team.ru>
2020-05-01 22:39:12 +0500

    redux

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Counter } from '../features/counter/Counter'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Counter></Counter>
+            </div>
+        )
+    }
+}
+
+export default App
